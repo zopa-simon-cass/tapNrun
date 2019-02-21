470707b8e9345bdb51ffc2b6e5180339d1702eec
beatricecourage <bea_courage@hotmail.co.uk>
2019-02-21 11:15:28 +0000

    Failing feature test for distance travelled by player

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -1,11 +1,20 @@
 describe("Player", () => {
 
+  beforeEach(() =>{
+      simon = new Player();
+    });
+
   describe("Start position", () => {
 
     it("starting positions is zero", () => {
-      var simon = new Player();
       expect(simon.position).toEqual(0);
     });
+
+    it("distance travelled is initially zero", () => {
+      expect(simon.distance).toEqual(0);
+    });
+
+
 
   });
 
