5ecbf22dce98d886615c04aa83a6a807662fd6cd
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-21 10:59:39 +0000

    test: passing test for new instance of

--- spec/timerSpec.js
+++ spec/timerSpec.js
@@ -3,10 +3,11 @@ describe("Timer", function() {
   var timer
 
   beforeEach(function() {
-    timer = new Timer()
+    timer = new Timer
   })
 
   it("can be instantiated", function() {
-    expect(timer).toBeAnInstanceOf(Timer)
+    console.log(timer)
+    expect(timer instanceof Timer).toBeTruthy()
   })
 })
