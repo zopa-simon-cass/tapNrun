db2ae8a304e9c3ad4f1182b70a5be8f79408c3bf
beatricecourage <bea_courage@hotmail.co.uk>
2019-02-22 12:22:41 +0000

    Tests passing, player has x and y coordinates

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -1,50 +1,83 @@
 describe("Player", () => {
 
-  beforeEach(() =>{
-      simon = new Player();
+  describe("x and y ctx coordinates", () => {
+
+    beforeEach(() => {
+      tom = new Player();
+    })
+
+    it("player has x coordinates", () => {
+      expect(tom.x).toEqual(jasmine.any(Number));
     });
 
-  describe("Start position", () => {
-
-    it("starting positions is zero", () => {
-      expect(simon.position).toEqual(0);
-    });
-
-    it("distance travelled is initially zero", () => {
-      expect(simon.distance).toEqual(0);
-    });
-
-  });
-
-  describe("Movement", () => {
-
-    beforeEach(() =>{
-        simon.move();
-      });
-
-
-    it("player moves a distance", () => {
-      expect(simon.distance).toEqual(1);
-    });
-
-    it("player moves a position", () => {
-      expect(simon.position).toEqual(1);
-    });
-
-    it ("Player moves distance cumulatively", () => {
-      simon.move()
-      expect(simon.distance).toEqual(2)
-    });
-
-    it ("Player position moves cumulateively", () => {
-      simon.move()
-      simon.move()
-      expect(simon.position).toEqual(3)
+    it("player has y  coordinates", () => {
+      expect(tom.y).toEqual(jasmine.any(Number));
     });
 
 
+  })
+
+})
 
 
-  });
 
-});
+
+
+
+
+
+
+
+
+
+
+// describe("Player", () => {
+//
+//   beforeEach(() =>{
+//       simon = new Player();
+//     });
+//
+//   describe("Start position", () => {
+//
+//     it("starting positions is zero", () => {
+//       expect(simon.position).toEqual(0);
+//     });
+//
+//     it("distance travelled is initially zero", () => {
+//       expect(simon.distance).toEqual(0);
+//     });
+//
+//   });
+//
+//   describe("Movement", () => {
+//
+//     beforeEach(() =>{
+//         simon.move();
+//       });
+//
+//
+//     it("player moves a distance", () => {
+//       expect(simon.distance).toEqual(1);
+//     });
+//
+//     it("player moves a position", () => {
+//       expect(simon.position).toEqual(1);
+//     });
+//
+//     it ("Player moves distance cumulatively", () => {
+//       simon.move()
+//       expect(simon.distance).toEqual(2)
+//     });
+//
+//     it ("Player position moves cumulateively", () => {
+//       simon.move()
+//       simon.move()
+//       expect(simon.position).toEqual(3)
+//     });
+//
+//
+//
+//
+//   });
+//
+// });
--- src/Player.js
+++ src/Player.js
@@ -1,14 +1,37 @@
-function Player(position = 6, distance= 0) {
-  this.position = position
-  this.distance = distance
+class Player {
+  constructor() {
+    this.x = 100
+    this.y = 100
+  }
 }
 
 
 
-Player.prototype.move = function() {
-  this.distance++;
-  this.position++;
-}
 
 
-increment ctx x value by a fixed distance,
+
+
+
+
+
+
+
+
+
+
+
+
+// function Player(position = 6, distance= 0) {
+//   this.position = position
+//   this.distance = distance
+// }
+//
+//
+//
+// Player.prototype.move = function() {
+//   this.distance++;
+//   this.position++;
+// }
+//
+//
+// increment ctx x value by a fixed distance,
