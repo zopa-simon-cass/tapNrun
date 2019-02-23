9f7b54cb8fbd0acb9fedf5741bd01981e4ef6f48
Tom Russell <tomrus@toms-mbp.home>
2019-02-23 15:38:41 +0000

    Driver switch

--- src/display.js
+++ src/display.js
@@ -1,34 +1,33 @@
 class Display {
   constructor() {
-    this.canvas = document.querySelector("canvas");
+    this.canvas = document.querySelector('canvas');
     this.player = new Player();
-    this.playerInit();
+    this.playerDraw();
   }
 
-  playerInit() {
-      this.canvas.querySelector("canvas");
-      var ctx = this.canvas.getContext('2d');
-      ctx.beginPath();
-      ctx.clearRect(0, 0, 500, 500)
-      ctx.arc(this.player.x, this.player.y, 50, 0, 2 * Math.PI);
-      ctx.fill()
+  // this.canvas.addEventListener("keydown", this.player.move(), true)
+
+  playerDraw() {
+    var ctx = this.canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.clearRect(0, 0, 500, 500)
+    ctx.arc(this.player.x, this.player.y, 50, 0, 2 * Math.PI);
+    ctx.fill()
   }
 
-  // this.canvas.onkeyup = function(e) {
-  //   if(e.keyCode == 32) {
-  //     this.player.move()
+  // playerMove() {
+  //   this.canvas.onkeyup = function(e) {
+  //     if(e.keyCode == 32) {
+  //       this.player.move()
+  //     }
   //   }
   // }
 
-  // addEventListener("keydown", Player.move(), true)
 
 
 }
 
-
-document.querySelector("canvas").onload = function(){
-  // console.log("fuck");
+document.addEventListener("DOMContentLoaded", function(event) {
   display = new Display()
-  // console.log(display);
-  display.playerInit();
-};
+  display.playerDraw();
+});
