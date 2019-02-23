5d6248039328f3df295a9f734d5f9e872a435901
Simon Cass <scass1991@gmail.com>
2019-02-23 17:17:46 +0000

    removed old commented code

--- src/Player.js
+++ src/Player.js
@@ -2,13 +2,10 @@ class Player {
   constructor() {
     this.x = 100
     this.y = 100
-    // this.display = new Display
-    // this.movement = new Movement()
   }
-  //
+  
   move() {
     this.x += 5;
-    // this.display.playerDraw()
   }
 
 }
--- src/display.js
+++ src/display.js
@@ -1,12 +1,9 @@
 class Display {
   constructor() {
     this.canvas = document.querySelector('canvas');
-    // this.movement = new Movement();
     this.player = new Player
     this.playerDraw();
   }
-
-  // this.canvas.addEventListener("keydown", this.player.move(), true)
 
   playerDraw() {
     var ctx = this.canvas.getContext('2d');
@@ -15,16 +12,6 @@ class Display {
     ctx.arc(this.player.x, this.player.y, 50, 0, 2 * Math.PI);
     ctx.fill()
   }
-
-  // playerMove() {
-  //   this.canvas.onkeyup = function(e) {
-  //     if(e.keyCode == 32) {
-  //       this.player.move()
-  //     }
-  //   }
-  // }
-
-
 
 }
 
