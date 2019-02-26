70e0b24f4bf7e7a974eab7f3fc90ef48052ea609
Jacques <jacques.laffy@gmail.com>
2019-02-26 17:09:49 +0000

    Removed Console Logs and Fixed Timer Stop

--- src/timer.js
+++ src/timer.js
@@ -10,13 +10,11 @@ class Timer {
 	start () {
 		this.offset = Date.now()
 		this.interval = setInterval(this.update.bind(this), 10)
-    console.log("Starting")
 	}
 
 	stop () {
 		clearInterval(this.interval)
 		this.interval = null
-    console.log("Stopping")
 	}
 
 	reset () {
