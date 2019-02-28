356caa4d536b4dd75e72c379b0d74c74779630ec
Jacques <jacques.laffy@gmail.com>
2019-02-28 11:44:11 +0000

    drawCanvas function only calls on other functions

--- src/display.js
+++ src/display.js
@@ -3,16 +3,14 @@ class Display {
     this.canvas = document.querySelector('canvas');
     this.canvas.width = 900
     this.canvas.height = 500
-    // FINISH LINE NEEDS TO BE MADE ADAPTIVE/ NOT 150
     this.finishLine = 7.8/9 * this.canvas.width
     this.timer = new Timer
     this.player = new Player(-8, 35, 12)
     this.player2 = new Player(-8, 160, 12)
-    this.controller = new Controller
-    this.controller2 = new Controller
-    this.interval
     this.lane3runner = new Player(50, 332, 0.98)
     this.lane4runner = new Player(50, 450, 0.7)
+    this.controller = new Controller
+    this.interval
     this.stopwatch = setInterval(this.trackFinish.bind(this), 10)
     this.mouseOn = true
     this.myAudio = new Audio('./assets/Audio/raceon.mp3');
@@ -21,7 +19,6 @@ class Display {
     this.countTimer
     this.count = 3
     this.doCount = false
-    // this.timer.start()
   }
 
   startButton() {
@@ -42,27 +39,37 @@ class Display {
     this.interval = setInterval(this.drawCanvas.bind(this), 10)
   }
 
-  drawCanvas() {
-    this.myAudio.addEventListener('ended', function() {
+  playerDraw() {
+    var ctx = this.canvas.getContext('2d')
+    var p1 = new Image()
+    var p2 = new Image()
+    p1.src = ("./assets/Joao.png")
+    p2.src = ("./assets/Joao1.png")
+    ctx.beginPath()
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    ctx.drawImage(p1, this.player.x, this.player.y, 100, 100)
+    ctx.drawImage(p2, this.player2.x, this.player2.y, 100, 100)
+    ctx.arc(this.lane3runner.x, this.lane3runner.y, 40, 0, 2 * Math.PI);
+    ctx.arc(this.lane4runner.x, this.lane4runner.y, 40, 0, 2 * Math.PI);
+    ctx.fill()
+  }
+
+  playAudio() {
+    this.myAudio.addEventListener('ended', function () {
       this.currentTime = 0;
       this.play();
     }, false);
     this.myAudio.play();
-    var ctx = this.canvas.getContext('2d');
-    let img = new Image()
-    let img2 = new Image()
-    img.src = ("./assets/Joao.png")
-    img2.src = ("./assets/Joao1.png")
-    ctx.beginPath()
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    ctx.drawImage(img, this.player.x, this.player.y, 100,100 )
-    ctx.drawImage(img2, this.player2.x, this.player2.y, 100,100 )
-    ctx.arc(this.lane3runner.x, this.lane3runner.y, 40, 0, 2 * Math.PI);
-    ctx.arc(this.lane4runner.x, this.lane4runner.y, 40, 0, 2 * Math.PI);
-    ctx.fill()
+  }
+
+  timerShow() {
+    var ctx = this.canvas.getContext('2d')
     ctx.font = "30px sans-serif";
     ctx.fillText(this.timer.formatTime(), 745, 490)
-    this.mouseOn = false
+  }
+
+  finishTimeShow() {
+    var ctx = this.canvas.getContext('2d')
     if (this.player.x > this.finishLine && this.time1=== null) {
       this.time1 = this.timer.formatTime()
       ctx.fillText(this.time1, 650, 95)
@@ -81,10 +88,24 @@ class Display {
     if (this.lane4runner.x > 845) {
       ctx.fillText("00:11:352", 650, 460)
     }
-    ctx.font = "130px sans-serif";
+  }
+
+  cdShow() {
+    var ctx = this.canvas.getContext('2d')
+    ctx.font = "130px sans-serif"
     if (this.doCount === true) {
       ctx.fillText(this.count + 1, 450, 250)
     }
+  }
+
+  drawCanvas() {
+    var ctx = this.canvas.getContext('2d')
+    this.playAudio()
+    this.playerDraw()
+    this.timerShow()
+    this.finishTimeShow()
+    this.cdShow()
+    this.mouseOn = false
     this.aiMovement();
   }
 
